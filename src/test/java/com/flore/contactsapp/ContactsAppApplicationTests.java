4f1233d375eb2fc865e459c63a02acfb191ca09c
Ricardo Flores <ricardo_fm77@ciencias.unam.mx>
2023-07-05 15:45:59 -0600

    initial commit

---
+++ src/test/java/com/flore/contactsapp/ContactsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flore.contactsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
